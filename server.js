d0ba28d99cbc5eed326660ca78c307c0cc96bd4f
samchat2561 <samchat2561@gmail.com>
2026-06-30 15:34:17 +0700

    web server

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = "exprees";
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    // res.send(`Hello,Welcome to my API!`);
+    res.status(200).json({ message: 'Welcome to my API!' })
+});
+
+//http://localhost:3000/api/users
+app.get('/api/users', (req, res) => {
+    res.status(200).json([
+        { id: 1, name: 'Somchai' },
+        { id: 2, name: 'Somsak' }
+    ]);
+});
+
+//http://localhost:3000/api/products
+app.get('/api/products', (req, res) => {
+    res.status(200).json([
+        { id: 101, name: 'Notebook', price: 350 }
+    ]);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
